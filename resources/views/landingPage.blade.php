11f8e2bc11af828a8054e4e50cd92e8c8098aa1f
Miyka96 <micaelaseminara@outlook.it>
2022-04-11 16:01:30 +0200

    landing page

---
+++ resources/views/landingPage.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Homepage</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: rgba(255, 255, 255, 0.741);
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+                color: grey;
+                box-sizing: border-box;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+            }
+            ul{
+                display: flex;
+                gap: 20px;
+            }
+            a{
+                text-decoration: none;
+                color: initial;
+            }
+            li{
+                list-style:none;
+                text-decoration:underline dashed 1px;
+                font-weight: 500;
+                font-size: 18px;
+            }
+        </style>
+    </head>
+
+    <body>
+       <h1>Welcome!</h1>
+       <h3>Please, select a playlist:</h3>
+       <ul>
+           <li><a href="#">Classic</a></li>
+           <li><a href="#">Rock</a></li>
+           <li><a href="#">Progressive</a></li>
+           <li><a href="#">Jazz</a></li>
+       </ul>
+    </body>
+</html>
